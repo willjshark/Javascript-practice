536025f5b7c8c1ad0e7d29df0e9644ff6cc7b04e
Sigita <sigita.zeb92@gmail.com>
2021-11-25 13:15:23 +0000

    Control thermostat using commands 'up' and 'down'

--- cli.js
+++ cli.js
@@ -3,3 +3,16 @@ const Thermostat = require("./thermostat");
 
 let thermostat = new Thermostat();
 console.log(`Temperature is ${thermostat.getTemperature()}`);
+
+while (true) {
+  let command = readlineSync.question("Enter command > ");
+  let result = `Temperature is ${thermostat.getTemperature()}`;
+
+  if (command === "up") {
+    thermostat.up();
+    console.log(`Temperature is ${thermostat.getTemperature()}`);
+  } else if (command === "down") {
+    thermostat.down();
+    console.log(`Temperature is ${thermostat.getTemperature()}`);
+  }
+}
